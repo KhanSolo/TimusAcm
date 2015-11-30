1073b4426a5e47bb5725245650c359f1a47381e3
smithy1985 <drpdrp@mail.ru>
2015-12-01 00:24:43 +0500

    FIX : 1197

--- Acm1197/main.cpp
+++ Acm1197/main.cpp
@@ -3,9 +3,20 @@
 #include <string>
 using namespace std;
 
+// ходы конём
 void main()
 {
-	int board[8][8] = {2,3,};
+	int board[8][8] = 
+	{ 
+		{ 2,3,4,4,4,4,3,2 },
+		{ 3,4,6,6,6,6,4,3 },
+		{ 4,6,8,8,8,8,6,4 },
+		{ 4,6,8,8,8,8,6,4 },
+		{ 4,6,8,8,8,8,6,4 },
+		{ 4,6,8,8,8,8,6,4 },
+		{ 3,4,6,6,6,6,4,3 },
+		{ 2,3,4,4,4,4,3,2 },
+	};
 
 	int n;
 	cin >> n;
@@ -21,15 +32,12 @@ void main()
 
 	while(!pos.empty())
 	{
-		string val = pos.back();
+		string val = pos.front();
 		pos.pop();
 
-		char c1= val[0];
-		char c2 = val[1];
+		char c1 = val[0] - 'a'; // 97
+		char c2 = val[1] - '1'; // 49
 
-
-	}
-
-
-	cout << endl;
+		cout << board[c1][c2] << endl;
+	}	
 }
