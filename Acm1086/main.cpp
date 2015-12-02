7832a63c2ea7c86908a79258601a580b9300d1f5
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-02 17:32:59 +0500

    ADD : 1086 solved

--- Acm1086/main.cpp
+++ Acm1086/main.cpp
@@ -1,42 +1,92 @@
-/*
-
-my $n=shift or die "$0: ”кажите число в командной строке\n";
-
-my @sieve=2..$n;
-
-while(@sieve)
-{
-	while(@sieve and $sieve[0]==0)
-	{
-		shift @sieve;
-	}
-	last unless @sieve;
-	print my $p=shift @sieve, "\n";
-
-	for(my $i=$p-1; $i<@sieve; $i+=$p)
-	{
-		$sieve[$i]=0;
-	}
-}
-
-*/
-
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class Simple
 {
-private:
+private:	
+	vector<int> cache;
+	int m_limit;
+	vector<bool> is_prime;
 
+	void dosieve(int limit)
+	{
+		int k, n;
+		
+		for (int i = 0; i < limit; i++)
+			is_prime[i] = false;
+
+		int x_lim = (int)trunc(sqrt(limit));
+		int y_lim = (int)trunc(sqrt(limit));
+
+		for (int x = 1; x <= x_lim; x++)
+			for (int y = 1; y <= y_lim; y++)
+			{
+				n = 4 * x*x + y*y;
+
+				if ((n <= limit) &&((n % 12 == 1) || (n % 12 == 5))) 
+					is_prime[n] = !is_prime[n];
+
+				n = n - x*x;
+
+				if ((n <= limit) &&(n % 12 == 7)) 
+					is_prime[n] = !is_prime[n];
+
+				n = n - 2 * y*y;
+
+				if ((x>y) &&(n <= limit) && (n % 12 == 11)) 
+					is_prime[n] = !is_prime[n];
+			}
+
+		int i_lim = (int)trunc(sqrt(limit));
+		for (int i = 5; i <= i_lim; i++)
+		{
+			if (is_prime[i])
+			{
+				k = i*i;
+				n = k;
+				while (n <= limit)
+				{
+					is_prime[n] = false;
+					n = n + k;
+				}
+			}
+
+		}
+		is_prime[2] = true;
+		is_prime[3] = true;
+	}
+	
+//----------
 public:
-	int process(int val);
+	
+	Simple(int limit) : 
+		m_limit(limit),
+		is_prime(limit+1)
+	{
+		// build
+		dosieve(m_limit);
+
+		cache.resize(15001); // dirty hack
+		int counter = 0;
+		for (int i = 1; i <= limit; i++)
+		{
+			if (is_prime[i] == true)
+			{
+				counter++;
+				cache[counter] = i;
+			}
+		}
+	}
+
+	int get_num(int val);
 };
 
-int Simple::process(int val)
+int Simple::get_num(int val)
 {
-	
+	return cache[val];
 }
 
 void main()
@@ -48,7 +98,12 @@ void main()
 	for (int i = 0; i < n; i++)	
 		cin >> v[i];	
 
-	Simple sim;
-	for (int i = 0; i < n;i++)	
-		cout << (sim.process(v[i]));	
+	// 15000e простое число - 163841
+	Simple sim(163841);
+	for (int i = 0; i < n; i++)
+	{
+		int vi = v[i];
+		int v = sim.get_num(vi);
+		cout << v << endl;
+	}
 }
