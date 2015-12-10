b7671e742b4ea023c9e45c25c34069a39d8deee3
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-10 12:05:51 +0500

    FIX : 1209 solved

--- Acm1209/main.cpp
+++ Acm1209/main.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
 
-typedef unsigned int uint;
+typedef long long uint; // long long, ò.ê. 8*ki 
+
+// 2^31 - 1 = 2147483647
+
+// 65536 * 65535 / 2 + 1 = 2147450881
+
+// 2147450881  => d = 17179607041 => sqrt = 131071 
 
 void main()
 {
-	int n; cin >> n;
+	int n; 
+	cin >> n;
 
 	vector<uint> k(n);
 
+	for (int i = 0; i < n; i++) cin >> k[i];		
+	
 	for (int i = 0; i < n; i++)
 	{
-		uint ki; 
-		cin >> ki;
-		k[i] = ki;
-	}
+		uint ki = k[i];
 
-	map<uint, int> m;
+		// x(x-1) / 2 + 1 = ki;
+		// x = (-1 + sqrt(1-8(1-ki))) / 2;
 
-	uint limit = 2147483647; // 2^31 - 1 
-	for (uint i = 1; i <= limit; i++)
-	{
-		uint c = (i - 1)*i / 2 + 1;
-		m[c] = 1;
-		//m.insert();
-	}
+		uint d = 1 - 8 * (1 - ki); // discriminant
+		
+		uint sq = (uint)sqrt(d);
 
-	for (int i = 0; i < n; i++)
-	{
-		if (m.count(k[i]))
+		if(sq*sq == d)
 			cout << 1 << " ";
 		else
 			cout << 0 << " ";
 	}
-
 }
