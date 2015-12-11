d56f45b490809a2d82a603fc5ee57194a51b298c
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-11 18:46:01 +0500

    TECH : 1234 still wa16

--- Acm1234/main.cpp
+++ Acm1234/main.cpp
@@ -4,57 +4,92 @@
 #include <string>
 using namespace std;
 
-typedef long double dbl; // sizeof(double) == sizeof(long double)
+typedef long double dbl;
+typedef long long lng;
 
-string GetAnswer(dbl a, dbl b, dbl c, dbl d, dbl e)
-{
-	//a *= 10.0;
-	//b *= 10.0;
-	//c *= 10.0;
-	//d *= 10.0;
-	//e *= 10.0;
-
-	dbl epsilon = 10e-9; // accuracy
-	
-	if (a<(d + epsilon)
-		&& 
-		b<(e+epsilon))
-	{		
+string GetAnswer(lng a, lng b, lng d, lng e)
+{	
+	if (a<=d
+		&&
+		b<=e)
+	{
 		return "YES";
 	}
-
-	/*
-	// diagonal angle - phy
-	dbl hyp = sqrtl(d*d + e*e);
-
-	dbl sin_phy = d / hyp;
-	dbl cos_phy = e / hyp;
-
-
-	dbl x = b*sin_phy + a*cos_phy; // (b * d + a * e) / sqrtl(d*d + e*e)
-	dbl y = b*cos_phy + a*sin_phy; // (b * e + a * d) / sqrtl(d*d + e*e)
-	*/
-
-	dbl x2 = (b * d + a * e)*(b * d + a * e) / (d*d + e*e); // (b*b*d*d + 2*b*d*a*e + a*a*e*e)
-	dbl y2 = (b * e + a * d)*(b * e + a * d) / (d*d + e*e);
-
-	//if (x <= d && y <= e)
-	if (x2 < (d*d + epsilon) && y2 < (e*e + epsilon))
-	{		
+	
+	//dbl x2 = (b * d + a * e)*(b * d + a * e) / (d*d + e*e); 
+	//dbl y2 = (b * e + a * d)*(b * e + a * d) / (d*d + e*e); 
+	if (
+	 (d*d*(d*d + e*e) >= (b * d + a * e)*(b * d + a * e)) &&
+	 (e*e*(d*d + e*e) >= (b * e + a * d)*(b * e + a * d))
+	)
+	//if (x2 <= (d*d) && y2 <= (e*e))
+	{
 		return "YES";
 	}
 	return "NO";
 }
 
-void swap(dbl&a, dbl&b) { dbl temp = a; a = b; b = temp; }
+//string GetAnswer(dbl a, dbl b, dbl c, dbl d, dbl e)
+//{
+//	dbl epsilon = 10e-9; // accuracy
+//	
+//	if (a<(d + epsilon)
+//		&& 
+//		b<(e+epsilon))
+//	{		
+//		return "YES";
+//	}
+//
+//	/*
+//	// diagonal angle - phy
+//	dbl hyp = sqrtl(d*d + e*e);
+//
+//	dbl sin_phy = d / hyp;
+//	dbl cos_phy = e / hyp;
+//
+//
+//	dbl x = b*sin_phy + a*cos_phy; // (b * d + a * e) / sqrtl(d*d + e*e)
+//	dbl y = b*cos_phy + a*sin_phy; // (b * e + a * d) / sqrtl(d*d + e*e)
+//	*/
+//
+//	dbl x2 = (b * d + a * e)*(b * d + a * e) / (d*d + e*e); // (b*b*d*d + 2*b*d*a*e + a*a*e*e) / (d*d + e*e)
+//	dbl y2 = (b * e + a * d)*(b * e + a * d) / (d*d + e*e); // 
+//
+//	//if (x <= d && y <= e)
+//	if (x2 < (d*d + epsilon) && y2 < (e*e + epsilon))
+//	{		
+//		return "YES";
+//	}
+//	return "NO";
+//}
 
-int main()
+void swap(dbl&a, dbl&b) { dbl temp = a; a = b; b = temp; }
+void swap(lng&a, lng&b) { lng temp = a; a = b; b = temp; }
+
+int main() // still wa 16
 {	
-	_ASSERT(GetAnswer(1.0, 1.5, 2.0, 1.0, 1.4) == "NO");	
-	_ASSERT(GetAnswer(1.0, 1.5, 2.0, 1.0, 1.5) == "YES");	
-	_ASSERT(GetAnswer(1.0, 10, 10, 6.0, 9.0) == "NO");
-	_ASSERT(GetAnswer(1.0, 10, 10, 7.0, 9.0) == "YES");
-	_ASSERT(GetAnswer(1.5, 2.9, 10.0, 1.4, 10.0) == "NO");
+	//dbl dd = 14.999999999999997;
+	//lng ll = (lng)dd;
+
+	//_ASSERT(GetAnswer(1.0, 1.0, 1.0, 1.0, 1.0) == "YES");
+	//_ASSERT(GetAnswer(1.0, 1.5, 2.0, 1.0, 1.4) == "NO");	
+	//_ASSERT(GetAnswer(1.0, 1.5, 2.0, 1.0, 1.5) == "YES");	
+	//_ASSERT(GetAnswer(1.0, 10.0, 10.0, 6.0, 9.0) == "NO");
+	//_ASSERT(GetAnswer(1.0, 10.0, 10.0, 7.0, 9.0) == "YES");
+	//_ASSERT(GetAnswer(1.5, 2.9, 10.0, 1.4, 10.0) == "NO");
+	//_ASSERT(GetAnswer(1.0, 10.0, 10.0, 9.0, 9.0) == "YES");
+	//_ASSERT(GetAnswer(10.0, 10.0, 10.0, 10.0, 10.0) == "YES");
+	//---
+
+	//_ASSERT(GetAnswer(10, 10, 10, 10) == "YES");
+	//_ASSERT(GetAnswer(10, 15, 10, 14) == "NO");
+	//_ASSERT(GetAnswer(10, 15, 10, 15) == "YES");
+	//_ASSERT(GetAnswer(10, 100, 60, 90) == "NO");
+	//_ASSERT(GetAnswer(10, 100, 70, 90) == "YES");
+	//_ASSERT(GetAnswer(15, 29, 14, 100) == "NO");
+	//_ASSERT(GetAnswer(10, 100, 90, 90) == "YES");
+	//_ASSERT(GetAnswer(100, 100, 100, 100) == "YES");
+
 	//---
 	
 	vector<dbl> v1(3);
@@ -69,7 +104,12 @@ int main()
 	dbl a = v1[0];
 	dbl b = v1[1];
 
-	string answer = GetAnswer(a, b, 0.0, d, e);
+	string answer = GetAnswer(
+		(lng)(10 * a), 
+		(lng)(10 * b), 
+		(lng)(10 * d), 
+		(lng)(10 * e)
+		);
 
 	cout << answer;
 
