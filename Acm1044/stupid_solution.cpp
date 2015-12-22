80bbca5d97c7324d8f2a34ff961a5d8992cfa875
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-22 19:49:25 +0500

    TECH : 1044 stupid solution

---
+++ Acm1044/stupid_solution.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	switch (N)
+	{
+	case 2: printf("10\n"); return 0;
+	case 4: printf("670\n"); return 0;
+	case 6: printf("55252\n"); return 0;
+	case 8: printf("4816030\n"); return 0;
+	default: return 0;
+	}
+	return 0;
+}
