2784b3ebe3a77f8beb394271ae01066156923ba4
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-23 11:38:45 +0500

    CHG : 1044 solved

--- Acm1044/main.cpp
+++ Acm1044/main.cpp
@@ -2,8 +2,49 @@
 
 using namespace std;
 
+int inline sum_of_digits(int x)
+{
+	int res = 0;
+
+	while (x > 0)
+	{
+		res += (x % 10);
+		x = x / 10;
+	}
+
+	return res;
+}
+
 int main()
 {
+	int n;
+	cin >> n;
+	
+	int k = 1; 
+	int t = (n / 2);
+
+	for (int i = 1; i <= t; i++, k *= 10);
+
+	k--;
+
+	int maxSum = t * 9;
+
+	int s = 0;
+	for (int i = 0; i <= maxSum; i++)
+	{
+		int c = 0;
+		for (int j = 0; j<= k;j++)
+		{
+			if (i == sum_of_digits(j)) 
+				c++;
+		}
+		s += c*c;
+	}
+
+	if (1 == (n % 2))	
+		s *= 10;	
+		
+	cout << s;
 
 	return 0;
 }
--- Acm1044/stupid_solution.cpp
+++ Acm1044/stupid_solution.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main()
@@ -14,3 +15,4 @@ int main()
 	}
 	return 0;
 }
+*/
