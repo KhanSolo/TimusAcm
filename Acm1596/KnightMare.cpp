65785b11492db33457192ba04424eba7595aea76
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-25 19:45:36 +0500

    TECH : 1596 KnightMare

--- Acm1596/KnightMare.cpp
+++ Acm1596/KnightMare.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+const string NS = "No solution";
+
 int main()
 {
+	int n, m;
+	cin >> n >> m;
+	
+	if ((n*m) & 1 == 1)
+	{
+		cout << NS;
+	
+	}
+	else
+	{
+		// доска
+		vector<int> init_row(m, 0);
+		vector<vector<int>> board(n, init_row);
 
-	vector<int> init_row(n, 0);
-	vector<vector<int>> board(n, init_row);
 
+	}
 	return 0;
 }
