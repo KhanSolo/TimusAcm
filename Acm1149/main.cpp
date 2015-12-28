5f500f91c64f62a48120bf0e59094d41926799e0
tatarchuk_ki <tatarchuk_ki@datakrat.ru>
2015-12-28 15:51:54 +0500

    TECH : 1149

--- Acm1149/main.cpp
+++ Acm1149/main.cpp
@@ -11,7 +11,6 @@
 
 
 
-
 исходные данные	
 
 3
@@ -36,9 +35,25 @@
 
 */
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string get_expr(int n)
+{
+
+	return nullptr;
+}
 
 int main()
 {
+	_ASSERT(get_expr(3) == "((sin(1)+3)sin(1–sin(2))+2)sin(1–sin(2+sin(3)))+1");
+	_ASSERT(get_expr(9) == "((((((((sin(1)+9)sin(1-sin(2))+8)sin(1-sin(2+sin(3)))+7)sin(1-sin(2+sin(3-sin(4))))+6)sin(1-sin(2+sin(3-sin(4+sin(5)))))+5)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6))))))+4)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7)))))))+3)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8))))))))+2)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8+sin(9)))))))))+1");
+
+	int n; 	cin >> n;
+
+	cout << get_expr(n) << endl;
 
 	return 0;
 }
