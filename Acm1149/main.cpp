7c34023f6261b6836d98223c58e4fbec7c069202
smithy1985 <drpdrp@mail.ru>
2015-12-29 23:29:54 +0500

    CHG 1149 solved

--- Acm1149/main.cpp
+++ Acm1149/main.cpp
@@ -1,57 +1,70 @@
-﻿
-/*
-
-Пусть An = sin(1–sin(2+sin(3–sin(4+...sin(n))...)
-
-Обозначим Sn = (...(A1+n)A2+n–1)A3+...+2)An+1
-
-Для заданного N выведите выражение SN
-Исходные данные
-В единственной строке дано целое число N. 1 ≤ N ≤ 200
-
-
-
-исходные данные	
-
-3
-
-
-
-результат
-
-((sin(1)+3)sin(1–sin(2))+2)sin(1–sin(2+sin(3)))+1
-
---------------------------------------------------
-	(
-		(sin(1)+3) * sin(1–sin(2)) + 2
-	) 
-	* 
-	sin(1–sin(2+sin(3)))+1
-
------------------------------------------------------
-
-9
-((((((((sin(1)+9)sin(1-sin(2))+8)sin(1-sin(2+sin(3)))+7)sin(1-sin(2+sin(3-sin(4))))+6)sin(1-sin(2+sin(3-sin(4+sin(5)))))+5)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6))))))+4)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7)))))))+3)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8))))))))+2)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8+sin(9)))))))))+1
-
-*/
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
 
-
 string get_expr(int n)
 {
+	string result;
 
-	return nullptr;
+	for (int i = 0; i < n - 1; i++)
+		result.push_back('(');
+	
+	for (int i = 1; i < n; i++) 
+	{
+		for (int j = 1; j <= i; j++) 
+		{
+			result.append("sin(");
+			result.append(to_string(j));
+
+			if (j != i) 
+			{
+				if (j % 2 == 1) 									
+					result.push_back('-');				
+				else 				
+					result.push_back('+');				
+			}
+		}
+
+		for (int j = 0; j < i; j++) 		
+			result.push_back(')');
+		
+		result.push_back('+');
+		result.append(to_string(n + 1 - i));
+		result.push_back(')');
+	}
+
+	for (int i = 1; i <= n; i++) 
+	{
+		result.append("sin(");
+		result.append(to_string(i));
+		if (i != n) 
+		{
+			if (i % 2 == 1) 			
+				result.push_back('-');			
+			else 			
+				result.push_back('+');			
+		}
+	}
+
+	for (int i = 0; i < n; i++) 	
+		result.push_back(')');	
+		
+	result.append("+1");
+
+	return result;
 }
 
 int main()
-{
-	_ASSERT(get_expr(3) == "((sin(1)+3)sin(1–sin(2))+2)sin(1–sin(2+sin(3)))+1");
-	_ASSERT(get_expr(9) == "((((((((sin(1)+9)sin(1-sin(2))+8)sin(1-sin(2+sin(3)))+7)sin(1-sin(2+sin(3-sin(4))))+6)sin(1-sin(2+sin(3-sin(4+sin(5)))))+5)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6))))))+4)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7)))))))+3)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8))))))))+2)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8+sin(9)))))))))+1");
+{	
+	//string str3_1 = "((sin(1)+3)sin(1-sin(2))+2)sin(1-sin(2+sin(3)))+1";
+	//_ASSERT(0 == strcmp(get_expr(3).c_str(), str3_1.c_str()));
 
-	int n; 	cin >> n;
+	//string str9_1 = "((((((((sin(1)+9)sin(1-sin(2))+8)sin(1-sin(2+sin(3)))+7)sin(1-sin(2+sin(3-sin(4))))+6)sin(1-sin(2+sin(3-sin(4+sin(5)))))+5)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6))))))+4)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7)))))))+3)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8))))))))+2)sin(1-sin(2+sin(3-sin(4+sin(5-sin(6+sin(7-sin(8+sin(9)))))))))+1";
+	//_ASSERT(0 == strcmp(get_expr(9).c_str(), str9_1.c_str()));
+
+	int n; 	
+	cin >> n;
 
 	cout << get_expr(n) << endl;
 
