dcc0e2a91834af48a9922f54acc81415a061e7c7
Konstantin <drpdrp@mail.ru>
2025-08-06 10:55:31 +0300

    REF: Acm1196

--- Acm1196/csharp/Program.cs
+++ Acm1196/csharp/Program.cs
@@ -1,37 +1,36 @@
 using System;
 using System.Collections.Generic;
 
-namespace Acm1196
+namespace Acm1196;
+
+public static class MainClass
 {
-    public static class MainClass
+    public static void Main()
     {
-        public static void Main()
+        var count = int.Parse(Console.ReadLine());
+        var map = new HashSet<uint>(count);
+        uint prev = 0;
+        for (var i = 0; i < count; i++)
         {
-            var count = int.Parse(Console.ReadLine());
-            var map = new HashSet<uint>(count);
-            uint prev = 0;
-            for (var i = 0; i < count; i++)
+            var year = uint.Parse(Console.ReadLine());
+            if (year != prev)
             {
-                var year = uint.Parse(Console.ReadLine());
-                if (year != prev)
-                {
-                    map.Add(year);
-                    prev = year;
-                }
+                map.Add(year);
+                prev = year;
             }
-
-            count = int.Parse(Console.ReadLine());
-
-            uint matches = 0;
-            for (int i = 0; i < count; i++)
-            {
-                var year = uint.Parse(Console.ReadLine());
-                if (map.TryGetValue(year, out _))
-                {
-                    matches++;
-                }
-            }
-            Console.Write(matches);
         }
+
+        count = int.Parse(Console.ReadLine());
+
+        uint matches = 0;
+        for (int i = 0; i < count; i++)
+        {
+            var year = uint.Parse(Console.ReadLine());
+            if (map.TryGetValue(year, out _))
+            {
+                matches++;
+            }
+        }
+        Console.Write(matches);
     }
 }
